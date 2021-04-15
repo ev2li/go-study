23657f2944b003889108c92fb50045590d53d7b3
ev2li <ev2li9748@163.com>
2021-04-15 19:14:24 +0800

    "4-15"

--- basedata/ptr/main.go
+++ basedata/ptr/main.go
@@ -1,11 +1,29 @@
-package main
+ package main
 
+import "fmt"
 
 func main(){
 	test()
 }
 
+func modify(p *int){
+	fmt.Println(p)
+	*p = 10000
+}
 
 func test(){
+	var a int = 10
+	fmt.Println(&a)
 
+	var p *int
+	 p = &a
+	fmt.Println(*p)
+
+	 var b int = 400
+	 p = &b
+	 *p = 5
+	 fmt.Println(b)
+
+	 modify(&b)
+	fmt.Println(b)
 }
