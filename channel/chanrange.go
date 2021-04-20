11fe5bcaa991de9f4990817a0161a5bc157021e7
ev2li <ev2li9748@163.com>
2021-04-20 18:55:09 +0800

    419mac

---
+++ channel/chanrange.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	test501()
+}
+
+func test501(){
+	var ch chan int
+	ch = make(chan int, 1000)
+
+	for i := 0; i < 1000; i++ {
+		ch <- i
+	}
+
+	close(ch)
+
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
