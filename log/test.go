37adfa2770e3b505d6356974500df5006c040dc1
ev2li <ev2li9748@163.com>
2021-05-12 15:01:20 +0800

    mac 5-12

--- log/test.go
+++ log/test.go
@@ -16,7 +16,7 @@ func main() {
 	msg.Topic = "nginx_log"
 	msg.Value = sarama.StringEncoder("this is a good test, my message is good")
 
-	client, err := sarama.NewSyncProducer([]string{"10.240.221.17:9092"}, config)
+	client, err := sarama.NewSyncProducer([]string{"10.240.167.183:9092"}, config)
 	if err != nil {
 		fmt.Println("producer close, err:", err)
 		return
